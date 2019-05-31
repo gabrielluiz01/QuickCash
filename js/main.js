ebbb6406a9b92ecb2807723ebda44b9ec074e801
vai na web <vainaweb@gmail.com>
2019-05-31 20:04:51 -0300

    ajustes nos itens em js

--- js/main.js
+++ js/main.js
@@ -76,13 +76,62 @@ $(function(){
     $('.buttomContact').click(function(){
         $('.faleConosco').css('display','block');
     });
-    $('.cancel').click(function(){
+    $('.closejs').click(function(){
         $('.faleConosco').css('display','none');
     });
-    $('.cancel').click(function(){
+    $('.closejs').click(function(){
         $('.buttomContact').css('display','flex');
     });
     $('.buttomContact').click(function(){
         $('.buttomContact').css('display','none');
-    });
+    });   
 });
+
+$(document).ready(function(){
+    $(".mostrar").hide();
+    $(".ocultar").click(function(){
+        $(this).next(".mostrar").slideToggle(600);
+    });
+}); 
+$('.arrow').on('click', function() {
+    $(this).toggleClass('active');
+});
+$('.arrow--l-r').on('click', function() {
+    $(this).toggleClass('left right');
+});
+
+var slidesIncentivadores = 1;
+showIncentivadores(slidesIncentivadores);
+
+function plusIncentivadores(n){
+  showIncentivadores(slidesIncentivadores += n);
+}
+
+function showIncentivadores(n){
+  var i;
+  var x = document.getElementsByClassName("contEncouraging-container");
+  if (n > x.length) {slidesIncentivadores = 1}
+  if (n < 1 ) {slidesIncentivadores = x.length};
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slidesIncentivadores-1].style.display = "flex";
+};
+
+var slidesProjects = 1;
+showProjects(slidesProjects);
+
+function plusProjects(n){
+  showProjects(slidesProjects += n);
+}
+
+function showProjects(n){
+  var i;
+  var x = document.getElementsByClassName("contProject-container");
+  if (n > x.length) {slidesProjects = 1}
+  if (n < 1 ) {slidesProjects = x.length};
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slidesProjects-1].style.display = "flex";
+};
